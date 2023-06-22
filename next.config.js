58b01be5dac2a4d43473f269bf69f58df455b3dd
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-06-22 08:54:39 -0300

    Fix: Exposição das variáveis de ambiente

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-})
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig = withPWA({
+	env: {
+		NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+		NEXT_PUBLIC_AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+		NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+		NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+		NEXT_PUBLIC_STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+		NEXT_PUBLIC_MESSAGING_SENDER_ID:
+			process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+		NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+		NEXT_PUBLIC_MEASUREMENT_ID: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+	},
 	images: {
 		remotePatterns: [
 			{
